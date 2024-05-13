59b1332ccb60f395d2e3bef54801f68e7f9e0864
AlejandroSuero <asuerome@gmail.com>
2024-05-14 01:14:19 +0200

    feat(logger): modified output format

--- lua/freeze-code/utils/logger.lua
+++ lua/freeze-code/utils/logger.lua
@@ -19,53 +19,53 @@ local M = {}
 ---Display error message
 ---@param msg string: Message to display
 function M.err(msg)
-  vim.notify(msg, vim.log.levels.ERROR, { title = "FreezeCode" })
+  vim.notify(msg .. "\n", vim.log.levels.ERROR, { title = "FreezeCode" })
 end
 
 ---Display error message once
 ---@param msg string: Message to display
 function M.err_once(msg)
-  vim.notify_once(msg, vim.log.levels.ERROR, { title = "FreezeCode" })
+  vim.notify_once(msg .. "\n", vim.log.levels.ERROR, { title = "FreezeCode" })
 end
 
 ---Display warn message
 ---@param msg string: Message to display
 function M.warn(msg)
-  vim.notify(msg, vim.log.levels.WARN, { title = "FreezeCode" })
+  vim.notify(msg .. "\n", vim.log.levels.WARN, { title = "FreezeCode" })
 end
 
 ---Display log message
 ---@param msg string: Message to display
 function M.info(msg)
-  vim.notify(msg, vim.log.levels.INFO, { title = "FreezeCode" })
+  vim.notify(msg .. "\n", vim.log.levels.INFO, { title = "FreezeCode" })
 end
 
 ---Display formatted error message
 ---@param msg string|number: Format to display
 ---@param ... any: Format parameters
 function M.err_fmt(msg, ...)
-  vim.notify(string.format(msg, ...), vim.log.levels.ERROR, { title = "FreezeCode" })
+  vim.notify(string.format(msg .. "\n", ...), vim.log.levels.ERROR, { title = "FreezeCode" })
 end
 
 ---Display formatted error message once
 ---@param msg string|number: Format to display
 ---@param ... any: Format parameters
 function M.err_once_fmt(msg, ...)
-  vim.notify_once(string.format(msg, ...), vim.log.levels.ERROR, { title = "FreezeCode" })
+  vim.notify_once(string.format(msg .. "\n", ...), vim.log.levels.ERROR, { title = "FreezeCode" })
 end
 
 ---Display formatted warn message
 ---@param msg string|number: Format to display
 ---@param ... any: Format parameters
 function M.warn_fmt(msg, ...)
-  vim.notify(string.format(msg, ...), vim.log.levels.WARN, { title = "FreezeCode" })
+  vim.notify(string.format(msg .. "\n", ...), vim.log.levels.WARN, { title = "FreezeCode" })
 end
 
 ---Display formatted log message
 ---@param msg string|number: Format to display
 ---@param ... any: Format parameters
 function M.info_fmt(msg, ...)
-  vim.notify(string.format(msg, ...), vim.log.levels.INFO, { title = "FreezeCode" })
+  vim.notify(string.format(msg .. "\n", ...), vim.log.levels.INFO, { title = "FreezeCode" })
 end
 
 return M
