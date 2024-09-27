8e0bec3823f05468fb024941614a0f21d0bfb3e5
AlejandroSuero <asuerome@gmail.com>
2024-09-27 14:03:00 +0200

    fix(commands): `copy_cmd` will now work correctly
    As @chhatbarhiren commented on #18,
    redirections don't work with `vim.fn.system`
    unless passsed as string using
    `{ "sh", "-c", command }`.

--- lua/freeze-code/commands.lua
+++ lua/freeze-code/commands.lua
@@ -95,11 +95,7 @@ local copy_by_os = function(opts)
   local filename = vim.fn.expand(opts.output)
   if config.config.copy_cmd ~= "" then
     local command = string.gsub(config.config.copy_cmd, "filename", filename)
-    cmd = { command }
-    return vim.fn.system(cmd)
-  end
-  if vim.fn.executable("gclip") ~= 0 then
-    cmd = { "gclip", "-copy", "-f", filename }
+    cmd = { "sh", "-c", command }
     return vim.fn.system(cmd)
   end
   if is_win then
